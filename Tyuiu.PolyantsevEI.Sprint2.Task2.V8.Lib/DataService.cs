40bb48e7764a578720451b5532138f4f2c8772cc
Tyuiu.PolyantsevEI@outlook.com <qreft@mail.ru>
2023-10-06 00:09:48 +0500

    Task2

---
+++ Tyuiu.PolyantsevEI.Sprint2.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.PolyantsevEI.Sprint2.Task2.V8.Lib
+{
+    public class DataService : ISprint2Task2V8
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            if ((y == 2 && x > 8 && x < 12) || (y == 5 && x > 2 && x < 10) || (y == 6 && x > 2 && x < 14) || (y == 10 && x > 5 && x < 10) || (y == 11 && x > 2 && x < 10) || (y == 12 && x > 6 && x < 10) || (y == 3 && ((x > 2 && x < 6) || (x > 8 && x < 13))) || (y == 4 && ((x > 2 && x < 6) || (x > 8 && x < 12))) || (y == 7 && ((x > 2 && x < 7) || (x > 8 && x < 15))) || (y == 9 && ((x > 5 && x < 10) || (x > 11 && x < 14))) || (y == 8 && (x == 6 || x == 9 || x == 12 || x == 13)))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Tyuiu.PolyantsevEI.Sprint2.Task2.V8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PolyantsevEI.Sprint2.Task2.V8.Lib;
+
+namespace Tyuiu.PolyantsevEI.Sprint2.Task2.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Полянцев Е. И. | ИСТНб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Полянцев Егор Ильич | ИСТНб-23-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение координаты X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение координаты Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            if (res) Console.WriteLine("Точка находится в заштрихованной области");
+                else Console.WriteLine("Точка не находится в заштрихованной области");
+
+            Console.ReadKey();
+        }
+    }
+}
