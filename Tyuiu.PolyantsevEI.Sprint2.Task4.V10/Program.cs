2f161e922649b96dfb428dcd13cfb8e43f144bc3
Tyuiu.PolyantsevEI@outlook.com <qreft@mail.ru>
2023-10-16 01:23:11 +0500

    Task4

--- Tyuiu.PolyantsevEI.Sprint2.Task4.V10/Program.cs
+++ Tyuiu.PolyantsevEI.Sprint2.Task4.V10/Program.cs
@@ -17,28 +17,29 @@ namespace Tyuiu.PolyantsevEI.Sprint2.Task4.V10
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #2                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #5                                                              *");
-            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
             Console.WriteLine("* Выполнил: Полянцев Егор Ильич | ИСТНб-23-1                              *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
-            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+            Console.WriteLine("* переменных X, Y с клавиатуры.                                           *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Введите число:");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите порядковый номер месяца:");
-            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            double res = ds.Calculate(m, n);
-            Console.WriteLine("Дата предыдущего дня:" + res);
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("Z = " + res);
             Console.ReadKey();
         }
     }
